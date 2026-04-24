9c13a984565c4c52c73803c5fd73d94292ff2f66
Soumendu Bagchi <soumendubagchi@gmail.com>
2026-04-24 06:00:50 -0400

    sync xrd_ovito implementation with the main

--- src/matensemble/dynopro/task_lib/AnalysisSubprocess.py
+++ src/matensemble/dynopro/task_lib/AnalysisSubprocess.py
@@ -77,12 +77,23 @@ def AnalysisSubprocess(comm, input_params):
                                         file.write(f'{data.timestep} {prob} {input_params["compute_twist"]["target_window"][0]} {input_params["compute_twist"]["target_window"][1]}')
 
                 if 'compute_xrd' in input_params.keys():
-                        
-                        if input_params['compute_xrd']:
+
+                            if input_params['compute_xrd']:
                                 filetag = f'XRD_{data.timestep}'
-                                xrd_pattern = compute_diffraction.get_xrd_pattern(data, filetag)
+                                xrd_pattern = compute_diffraction.get_xrd_ovito(data, filetag)
                                 if rds is not None:
-                                        rds.register_on_stream(namespace=input_params['stream']['namespace'], key="xrd_data", timestep=data.timestep, xrd_pattern=xrd_pattern)
+                                        # Ensure XRD pattern is JSON-serializable before streaming
+                                        if isinstance(xrd_pattern, np.ndarray):
+                                                xrd_pattern_stream = xrd_pattern.tolist()
+                                        else:
+                                                xrd_pattern_stream = xrd_pattern
+                                        rds.register_on_stream(
+                                                namespace=input_params['stream']['namespace'],
+                                                key="xrd_data",
+                                                timestep=data.timestep,
+                                                xrd_pattern=xrd_pattern_stream,
+                                        )
+                        
 
                 if 'compute_Laue_Diffraction' in input_params.keys():
                         
